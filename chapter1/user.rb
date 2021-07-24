c585ed58e40e8c7b6fb400c11f0bb22b6f6e668d
Miyata_Daisuke <miyadailikebump@gmail.com>
2021-07-24 21:44:35 +0900

    1-3-6

--- chapter1/user.rb
+++ chapter1/user.rb
@@ -1,7 +1,19 @@
-class User
-  attr_accessor :name, :address, :email
-
-  def profile
-    "#{name}(#{address})"
-  end
+options = {font_size: 100, color: 'red'}
+options.each do |key, value|
+  puts key
+  puts value
 end
+puts "------------------------"
+options.each do |key|
+  puts key
+end
+puts "------------------------"
+options.each do |k, v|
+  puts v
+  puts k
+  puts "------------------------"
+end
+options.each do |a|
+  puts "#{a}"
+end
+puts "------------------------"
